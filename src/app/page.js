9776b8ea8243762f2f6bc890da6c6b6a520999e0
marcUs <mf8santos@gmail.com>
2023-11-11 22:25:37 -0300

    header layout finished

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import Header from "@/commom/components/header";
 export default function Home() {
   return (
     <main >
-      <Header layout="1" text="Clique Aqui" size="small"/>
+      <Header />
+
     </main>
   );
 }
