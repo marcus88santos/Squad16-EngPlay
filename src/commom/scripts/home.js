8de52d69dd8386c7d2b962f5e5fd35bd98f05aa8
marcUs <mf8santos@gmail.com>
2023-11-19 13:00:13 -0300

    home screen by sthefany

---
+++ src/commom/scripts/home.js
@@ -0,0 +1,20 @@
+{/* // Índice da vaga atual */}
+let currentIndex = 0; 
+  
+function next() {
+    const jobListing = document.getElementById("jobListing");
+    currentIndex = (currentIndex + 1) % jobListing.children.length;
+    updateVisibility();
+}
+
+function prev() {
+    const jobListing = document.getElementById("jobListing");
+    currentIndex = (currentIndex - 1 + jobListing.children.length) % jobListing.children.length;
+    updateVisibility();
+}
+
+function updateVisibility() {
+    const jobListing = document.getElementById("jobListing");
+    const width = jobListing.clientWidth;
+    jobListing.style.transform = `translateX(-${width * currentIndex}px)`;
+}
